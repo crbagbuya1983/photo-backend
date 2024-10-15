94b1cba5b99f3fe0d6f8b5c832dc32d72648e6d4
crbagbuya1983 <crbagbuya@gmail.com>
2024-10-15 16:02:40 -0700

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const uploadPhotoToS3 = async (file) => {
 };
 
 const corsOptions = {
-  origin: ['https://disney-world-memories-6ecykxg1s-chris-projects-0f0e5269.vercel.app'], // Replace with your frontend URL
+  origin: [process.env.VERCEL_FRONTEND_URL], // Replace with your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true, // If you need to allow credentials
 };
