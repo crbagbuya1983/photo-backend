09b86d208d58c991a92d204e2587c5fd930a5bbd
crbagbuya1983 <crbagbuya@gmail.com>
2024-10-15 16:41:06 -0700

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ const corsOptions = {
 };
 
 const app = express();
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 // app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
